96e658d1e1d2521d91fefb69116f6033905414dd
supermanxkq <994028591@qq.com>
2019-08-07 11:58:37 +0800

    '菜单点击更改显示样式代码提交，包括菜单下的子链接'

--- static/js/left/left.js
+++ static/js/left/left.js
@@ -1,20 +1,19 @@
 $(function () {
     var urlstr = location.href;　　　　//获取浏览器的url
-    console.log('当前访问的浏览器的urlstr的值为：' + urlstr);
     var urlstatus = false;　　　　　　　　//标记
     //遍历导航div
-
+    var url_method=urlstr.split('/')[3];
     $(".page-sidebar-menu li").each(function () {
 //判断导航里面的rel和url地址是否相等
         if ($(this).parent().hasClass("sub-menu")) {
-              if ((urlstr + '/').indexOf($(this).attr('rel')) > -1 && $(this).attr('rel') != '') {
+              if ($(this).attr('rel')&&$(this).attr('rel').indexOf(url_method) > -1 && $(this).attr('rel') != '') {
                   $(this).parent().parent().addClass('active');
                    $(this).addClass('active');
               }else{
                    $(this).removeClass('active');
               }
         } else {
-            if ((urlstr + '/').indexOf($(this).attr('rel')) > -1 && $(this).attr('rel') != '') {
+            if ($(this).attr('rel')&&$(this).attr('rel').indexOf(url_method) > -1 && $(this).attr('rel') != '') {
                 $(this).addClass('active');
                 urlstatus = true;
             } else {
